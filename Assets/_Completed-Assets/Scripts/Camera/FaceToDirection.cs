944a1277d9ede823e683cd84c39a0ead15469a14
Ca <ca@wolffungame.com>
2022-05-11 15:16:48 +0700

    Update

---
+++ Assets/_Completed-Assets/Scripts/Camera/FaceToDirection.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Complete
+{
+    public class FaceToDirection : MonoBehaviour
+    {
+        public Transform m_HealthTransform;
+        public Vector3 rotation;
+        void Update()
+        {
+            m_HealthTransform.rotation = Quaternion.Euler(rotation); 
+        }
+    }
+}
--- Assets/_Completed-Assets/Scripts/Tank/TankHealth/TankHealthUI.cs
+++ Assets/_Completed-Assets/Scripts/Tank/TankHealth/TankHealthUI.cs
@@ -11,14 +11,23 @@ namespace Complete
         public Image m_FillImage;                           // The image component of the slider.
         public Color m_FullHealthColor = Color.green;       // The color the health bar will be when on full health.
         public Color m_ZeroHealthColor = Color.red;         // The color the health bar will be when on no health.
+        public Text txtHealth;
+
 
         public void SetHealUI(float m_CurentHealth,float m_StartingHealth)
         {
+            txtHealth.text = (Mathf.RoundToInt(m_CurentHealth)).ToString();
             // Set the slider's value appropriately.
-            m_Slider.value = GetComponentInParent<TankHealthData>().m_CurrentHealth;
+            m_Slider.value = (m_CurentHealth / m_StartingHealth) * 100;
             // Interpolate the color of the bar between the choosen colours based on the current percentage of the starting health.
             m_FillImage.color = Color.Lerp(m_ZeroHealthColor, m_FullHealthColor, m_CurentHealth / m_StartingHealth);
         }
 
+
+        private void OnValidate()
+        {
+            if (!txtHealth)
+                txtHealth = GetComponentInChildren<Text>();
+        }
     }
 }
